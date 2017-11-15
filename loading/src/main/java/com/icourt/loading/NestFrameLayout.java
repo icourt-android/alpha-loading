01b9cab143df2a101187e65e5d9fa5aef49d8d09
吴佑炫 <youxuan@icourt.cc>
2017-11-15 13:52:45 +0800

    支持nestscroll

---
+++ loading/src/main/java/com/icourt/loading/NestFrameLayout.java
@@ -0,0 +1,94 @@
+package com.icourt.loading;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.NestedScrollingChild;
+import android.support.v4.view.NestedScrollingChildHelper;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * @author youxuan  E-mail:xuanyouwu@163.com
+ * @version 2.2.1
+ * @Description 支持嵌套滚动
+ * @Company Beijing icourt
+ * @date createTime：2017/11/15
+ */
+public class NestFrameLayout extends FrameLayout implements NestedScrollingChild {
+
+    private NestedScrollingChildHelper mChildHelper;
+
+    public NestedScrollingChildHelper getmChildHelper() {
+        if (mChildHelper == null) {
+            mChildHelper = new NestedScrollingChildHelper(this);
+        }
+        return mChildHelper;
+    }
+
+    private void init() {
+        mChildHelper = new NestedScrollingChildHelper(this);
+    }
+
+    public NestFrameLayout(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    public NestFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public NestFrameLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @Override
+    public void setNestedScrollingEnabled(boolean enabled) {
+        getmChildHelper().setNestedScrollingEnabled(enabled);
+    }
+
+    @Override
+    public boolean isNestedScrollingEnabled() {
+        return getmChildHelper().isNestedScrollingEnabled();
+    }
+
+    @Override
+    public boolean startNestedScroll(int axes) {
+        return getmChildHelper().startNestedScroll(axes);
+    }
+
+    @Override
+    public void stopNestedScroll() {
+        getmChildHelper().stopNestedScroll();
+    }
+
+    @Override
+    public boolean hasNestedScrollingParent() {
+        return getmChildHelper().hasNestedScrollingParent();
+    }
+
+    @Override
+    public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed,
+                                        int dyUnconsumed, int[] offsetInWindow) {
+        return getmChildHelper().dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed,
+                offsetInWindow);
+    }
+
+    @Override
+    public boolean dispatchNestedPreScroll(int dx, int dy, int[] consumed, int[] offsetInWindow) {
+        return getmChildHelper().dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow);
+    }
+
+    @Override
+    public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
+        return getmChildHelper().dispatchNestedFling(velocityX, velocityY, consumed);
+    }
+
+    @Override
+    public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
+        return getmChildHelper().dispatchNestedPreFling(velocityX, velocityY);
+    }
+}
